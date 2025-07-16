16c40ffe8a933e5c3737f51e8b211f546b21ca4f
Blossom Ami <bloosmami@gmail.com>
2025-07-16 22:08:56 +0330

    review section is 80% done - footer is 100% done.

--- js/main.js
+++ js/main.js
@@ -39,3 +39,32 @@ closeBtn.addEventListener('click', () => {
     closeBtnIcn.classList.toggle(arrowLeftClass);
 });
 
+/*--------- SWIPER -----------*/
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 400,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disableOnIntraction: false
+    },
+
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+    grabCursor: true,
+    breakpoints: {
+        640: {
+            slidesPerView: 1
+        },
+        768: {
+            slidesPerView: 2
+        },
+        1024: {
+            slidesPerView: 3
+        },
+    }
+});
